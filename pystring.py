a27dab82aac7b1d2783322939217c06929681f99
js-god <39174891+js-god@users.noreply.github.com>
2022-04-20 06:15:16 +1000

    first mod

--- pystring.py
+++ pystring.py
@@ -1,10 +1,14 @@
 name = "phon"
 print(f"hello {name}")
 
+mystr1 = "Hello Everyone2"
+for i in mystr1:
+    print(i, end="")
+print(end="\n")
+
 mystr1 = "Hello Everyone"
 for i in mystr1:
     print(i)
 
-mystr1 = "Hello Everyone2"
-for i in mystr1:
-    print(i, end="")
+
+
